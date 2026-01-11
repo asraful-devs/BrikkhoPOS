f5130a46f04c356ddb89878c83dbf2c39898aa71
MdAsraful56 <work.mdasraful56@gmail.com>
2026-01-12 02:17:56 +0600

    shadCN Install & setup

--- BrikkhoPOS Client Site/src/App.tsx
+++ BrikkhoPOS Client Site/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
     return (
         <>
-            <div className='text-red-800'>Hello</div>
+            <div className=''>
+                <Button>Click me</Button>
+            </div>
         </>
     );
 }
