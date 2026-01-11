fa4e32ccb92ef8cd4ffcfc0d61f8bd5cade9973a
MdAsraful56 <work.mdasraful56@gmail.com>
2026-01-11 10:04:21 +0600

    login completed

--- BrikkhoPOS Server Site/src/app/config/index.ts
+++ BrikkhoPOS Server Site/src/app/config/index.ts
@@ -8,4 +8,12 @@ export default {
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
     bcrypt_salt_rounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 12,
+    jwt: {
+        access_secret: process.env.JWT_ACCESS_SECRET,
+        refresh_secret: process.env.JWT_REFRESH_SECRET,
+        reset_token: process.env.JWT_RESET_TOKEN_SECRET,
+        access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+        refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+        reset_token_expires_in: process.env.JWT_RESET_TOKEN_EXPIRES_IN,
+    },
 };
